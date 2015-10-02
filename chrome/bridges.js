d8ef03fe25f9d965beb86ca72388cc84b0c18857
Griffin <griffin@cryptolab.net>
2015-10-02 12:54:36 +0200

    new bridges for Chrome

--- chrome/bridges.js
+++ chrome/bridges.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
-    var bridges = new Array('<input class="getacross" value="194.132.208.14:9968 8311405c99046b6b1628b9628f5f1e3e7daeb398">',
-'<input class="getacross" value="192.36.27.165:28864 8b37facf9449c6e49fb62820bfc307e5bd3a6377">',
-'<input class="getacross" value="77.20.221.213:443 25f931888f0275ebaba65f789b1e2bdd245f3bf3">',
-'<input class="getacross" value="87.122.56.159:443 94bb03a511f2564f70469853a97a4caa1be10675">',
-'<input class="getacross" value="128.78.143.81:443 107406948ff9f08d78cf0449354c5dd0c45ed62b">',
-'<input class="getacross" value="218.146.29.202:443 3dc000f6e2a19add46ba07028dce51df9ce220b2">',
-'<input class="getacross" value="54.84.89.219:443 d4e875fecd0bf023f148871b30d41805999602b4">',
-'<input class="getacross" value="198.23.200.135:443 0a3217df6c520377e627780448c71cfba921e1f2">',
-'<input class="getacross" value="194.132.208.247:29968 9937d1e58cedf0d7f23a0899483b304ef41133d4">',
-'<input class="getacross" value="5.178.64.142:443 8c945a4ee5331662a450d8de8e6c4e162f1871e3">',
-'<input class="getacross" value="192.36.27.30:20195 a6005c8c57b4d2fb63e2795c507676abf8a96314">',
-'<input class="getacross" value="94.34.149.127:9443 54b8748657afa7c94862a495ae1570f78de20cb1">');
+    var bridges = new Array('<input class="getacross" value="obfs3 192.36.27.73:57307 01A95FF9C3C7276C8D18889A102F86026A679026">',
+'<input class="getacross" value="obfs3 194.132.209.97:42356 8AE7001B106E453B4CC973C4D1EB76D6772B350A">',
+'<input class="getacross" value="obfs3 23.95.27.37:49209 3BDCDFE8E83B24331B58471450FFFA2B79A6B9AC">',
+'<input class="getacross" value="obfs3 45.33.80.72:37868 C058186571C5448AAE27B402150DCBCCC1ADAC1F">',
+'<input class="getacross" value="obfs3 192.36.27.254:58476 FBF8C2BB73753A5075306A624C5C4D605DCD502D">',
+'<input class="getacross" value="obfs3 83.212.117.231:54185 B687A4B74920A7842B676C0C300D7119FB5F7E24">',
+'<input class="getacross" value="37.139.80.10:9001 8CD7E6DE9E0DC3A281FE997CB73022D948047B6E">',
+'<input class="getacross" value="37.138.83.202:443 EF2CB29C5E440D61DEA21E9C357BE962503C6D0F">');
     var randno = Math.floor(Math.random()*(bridges.length));
     $('.bridge').append(bridges[randno]);
     console.log(randno);
